816cf768f88a32103e657890ecfb8c034c406550
Iván Ramos Muñoz <ivan.ramos@11paths.com>
2016-10-06 12:47:34 +0200

    Implements timeout in all functions

--- ScreenController.h
+++ ScreenController.h
@@ -19,21 +19,25 @@
 class ScreenController {
 
 	public:
-		ScreenController(LiquidCrystal _lcd, int width=16, int height=2);
+		ScreenController(LiquidCrystal _lcd, int width=16, int height=2, int _pinOnOff=-1);
+		void powerOn(LiquidCrystal _lcd);
+		void powerOff(LiquidCrystal _lcd);
 		void print(LiquidCrystal _lcd, String msg);
 		void println(LiquidCrystal _lcd, String msg);
 		void clear(LiquidCrystal _lcd);
-		int readButton();
-		void waitButton(int button);
+		int readButton(int timeout=-1);
+		void waitButton(int button, int timeout=-1);
+   		int waitButtons(int wait_buttons[], int timeout=-1);
 		int moveWindow(LiquidCrystal _lcd, int limitUp = 0, int limitDown = LIMIT_BUFFER-1);
 		void printItem(LiquidCrystal _lcd, String msg);
 		void printSubItem(LiquidCrystal _lcd, String msg);
 		void printCorner(LiquidCrystal _lcd, String msg);
 		void printList(LiquidCrystal _lcd, String titles[], int size, int selected=0);
-		int readItemList(LiquidCrystal _lcd, String titles[], int size);
-		float readFloat(LiquidCrystal _lcd, String title, float initValue, int decimal, String unit="");
-		int readInteger(LiquidCrystal _lcd, String title, int initValue, String unit="");
-		String readString(LiquidCrystal _lcd, String title, String initValue);
+		int readItemList(LiquidCrystal _lcd, String titles[], int size, int timeout=-1);
+		float readFloat(LiquidCrystal _lcd, String title, float initValue, int decimal, String unit="", int timeout=-1);
+		int readInteger(LiquidCrystal _lcd, String title, int initValue, String unit="", int timeout=-1);
+		String readString(LiquidCrystal _lcd, String title, String initValue, int timeout=-1);
+		String readStringNumber(LiquidCrystal _lcd, String title, String initValue, int maxLength=15, int timeout=-1);
 
 	//protected:
 		
@@ -43,8 +47,10 @@ class ScreenController {
 		int positionAbs(int posRelative);
 		void moveScreen(LiquidCrystal _lcd, int button, int limitUp = 0, int limitDown = LIMIT_BUFFER-1);
 		void printBuffer(LiquidCrystal _lcd);
+   		bool contains(int array[], int value);
 		String buffer[LIMIT_BUFFER];
 		int height;
+		int pinOnOff;
 		int writeY;
 		int windows_pos;
 		int init;
---
+++ examples/screen7/screen7.ino
@@ -0,0 +1,47 @@
+#include <LiquidCrystal.h>
+#include <ScreenController.h>
+
+#define NUM_MENUS (sizeof(titles)/sizeof(String))
+#define POWER_LCD 10
+#define TIMEOUT 5000
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+ScreenController screen(lcd, 16, 2, POWER_LCD);
+
+int selected = -1;
+String titles[5] = {"Menu 1", "Menu 2", "Menu 3", "Menu 4", "Menu 5"};
+int boton = BUTTON_NONE;
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  
+  String str = "ScreenController";
+  screen.println(lcd, str);
+  delay(2000);
+  str = "Test 7";
+  screen.print(lcd, str);
+  delay(2000);
+}
+
+void loop() {
+  boton = screen.readButton(200);
+  
+  if (boton != BUTTON_NONE) {
+    screen.powerOn(lcd);
+    screen.clear(lcd);
+    delay(100);
+    selected = screen.readItemList(lcd, titles, NUM_MENUS, TIMEOUT);
+    
+    if (selected != -1) {
+      screen.clear(lcd);
+      String output = "Valor: "+ String(selected+1);
+      screen.println(lcd, output);
+      screen.print(lcd, "Pulsa SELECT");
+      screen.waitButton(BUTTON_SELECT, TIMEOUT);
+      delay(200);
+    }
+  }
+  
+  screen.powerOff(lcd);  
+}
